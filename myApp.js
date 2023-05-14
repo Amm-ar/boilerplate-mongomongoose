80a4cf383ba5d70eb10a70598a3fe85926513d3f
amm-ar <amar9683@outlook.com>
2023-05-14 15:42:00 +0200

    Use model.findOne() to Return a Single Match

--- myApp.js
+++ myApp.js
@@ -44,7 +44,10 @@ const findPeopleByName = (personName, done) => {
 };
 
 const findOneByFood = (food, done) => {
-  done(null /*, data*/);
+  Person.findOne({'favoriteFoods': food}, (err, data)=>{
+    if (err) return console.error("3 ",err);
+    done(null, data);
+  });
 };
 
 const findPersonById = (personId, done) => {
