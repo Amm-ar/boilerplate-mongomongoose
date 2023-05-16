934053d1c11159cbea136985b55d596d6adbaf6d
amm-ar <amar9683@outlook.com>
2023-05-16 16:29:43 +0200

    select function is too stubid you need to pass all of your selected fields in one string seprated by a space

--- myApp.js
+++ myApp.js
@@ -97,7 +97,7 @@ const queryChain = (done) => {
   Person.find({favoriteFoods: foodToSearch})
   .sort("name")
   .limit(2)
-  .select('name', 'favoriteFoods')
+  .select('name favoriteFoods')
   .exec(() => {
     if (err) return console.error("3 ",err);
     done(null, data);
